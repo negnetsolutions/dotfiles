362e91249dfaa847f69c2966f65a344dc3594809
Andrew Johnson <andy@negnetsolutions.com>
2011-12-26 20:47:06 -0600

    fixed install script problem

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+git submodule init
+git submodule update
+
 #install zsh
 wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 echo "source ~/.dotfiles/zsh/zshrc" >> ~/.zshrc
@@ -7,9 +10,6 @@ echo "source ~/.dotfiles/zsh/zshrc" >> ~/.zshrc
 #install vim
 ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
 cd ~/.dotfiles/vim
-
-git submodule init
-git submodule update
 
 # update command-t
 cd bundle/wincent-Command-T
